de60a6b5bde57c525205b03e61f3a074bed71f5a
Ovidio R. Calvet <calveto@gmail.com>
2018-05-31 19:35:59 -0400

    add matches page

---
+++ src/MatchesPage/MatchesPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MatchesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Matches Page</h1>
+      </div>
+    )
+  }
+}
+
+export default MatchesPage;
