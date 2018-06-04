ae4863e7d0edcec8530415c430768c7c2c20a2c8
Ovidio R. Calvet <calveto@gmail.com>
2018-06-04 17:38:10 -0400

    change default weather to loading

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ export default () => {
   const store = createStore(reducers, {
     selections: {
       selectedWeek: new Date(2018, 5, 5),
-      weather: 'weather description'
+      weather: 'loading...'
     },
     matches: [{
       id: 1,
