bbbf5fdecb88d3077bac135c6b3961eee5da9028
Ovidio R. Calvet <calveto@gmail.com>
2018-06-06 21:54:58 -0400

    add schedule and teams data providers

---
+++ src/shared/providers/schedule.js
@@ -0,0 +1,53 @@
+export const getWeekSchedule = (weekNumber) => {
+  return Promise.resolve([{
+    side: 'Back',
+    week: weekNumber,
+    date: new Date(),
+    matches: [{
+      id: 1,
+      hole: 10,
+      team1: 1,
+      team2: 12
+    },{
+      id: 2,
+      hole: 11,
+      team1: 11,
+      team2: 2
+    },{
+      id: 3,
+      hole: 12,
+      team1: 9,
+      team2: 4
+    },{
+      id: 4,
+      hole: 13,
+      team1: 6,
+      team2: 7
+    },{
+      id: 5,
+      hole: 14,
+      team1: 5,
+      team2: 8
+    },{
+      id: 6,
+      hole: 15,
+      team1: 3,
+      team2: 10
+    },{
+      id: 7,
+      hole: 16,
+      team1: 18,
+      team2: 13
+    },{
+      id: 8,
+      hole: 17,
+      team1: 17,
+      team2: 14
+    },{
+      id: 9,
+      hole: 18,
+      team1: 16,
+      team2: 15
+    }]
+  }]);
+}
---
+++ src/shared/providers/teams.js
@@ -0,0 +1,223 @@
+export const getTeams = () => {
+  return Promise.resolve([{ 
+    id: 1, 
+    rank: 1,
+    players:[{ 
+      id: 1, 
+      name: 'Mike Hudik',
+      handicap: 14
+    }, { 
+      id: 2, 
+      name: 'Chris Denison',
+      handicap: 14
+    }]
+  },{ 
+    id: 2, 
+    rank: 1,
+    players:[{ 
+      id: 3, 
+      name: 'Chris Bennett',
+      handicap: 8
+    }, { 
+      id: 4, 
+      name: 'Robert Snyder',
+      handicap: 12
+    }]
+  },{ 
+    id: 3, 
+    rank: 1,
+    players:[{ 
+      id: 5, 
+      name: 'Mark Mileusnic',
+      handicap: 14
+    }, { 
+      id: 6, 
+      name: 'Damian England',
+      handicap: 8
+    }, { 
+      id: 7, 
+      name: 'Steve Sibner',
+      handicap: 13
+    }]
+  },{ 
+    id: 4, 
+    rank: 1,
+    players:[{ 
+      id: 8, 
+      name: 'John Dunagan',
+      handicap: 14
+    }, { 
+      id: 9, 
+      name: 'Larry Pomeranz',
+      handicap: 13
+    }]
+  },{ 
+    id: 5, 
+    rank: 1,
+    players:[{ 
+      id: 10, 
+      name: 'Rich Berger',
+      handicap: 15
+    }, { 
+      id: 11, 
+      name: 'James Parker',
+      handicap: 9
+    }]
+  },{ 
+    id: 6, 
+    rank: 1,
+    players:[{ 
+      id: 12, 
+      name: 'Lisa Jarnot',
+      handicap: 5
+    }, { 
+      id: 13, 
+      name: 'Walt Basinger',
+      handicap: 7
+    }]
+  },{ 
+    id: 7, 
+    rank: 1,
+    players:[{ 
+      id: 14, 
+      name: 'Madhu Akella',
+      handicap: 7
+    }, { 
+      id: 15, 
+      name: 'Gnaswar Challa',
+      handicap: 12
+    }]
+  },{ 
+    id: 8, 
+    rank: 1,
+    players:[{ 
+      id: 16, 
+      name: 'Greg Nichols',
+      handicap: 8
+    }, { 
+      id: 17, 
+      name: 'Dylan Drozdiwicz',
+      handicap: 9
+    }]
+  },{ 
+    id: 9, 
+    rank: 1,
+    players:[{ 
+      id: 18, 
+      name: 'James King',
+      handicap: 18
+    }, { 
+      id: 19, 
+      name: 'Chris Weber',
+      handicap: 14
+    }]
+  },{ 
+    id: 10, 
+    rank: 1,
+    players:[{ 
+      id: 20, 
+      name: 'Hal Venoff',
+      handicap: 16
+    }, { 
+      id: 21, 
+      name: 'Mary DeMayo',
+      handicap: 10
+    }]
+  },{ 
+    id: 11, 
+    rank: 1,
+    players:[{ 
+      id: 22, 
+      name: 'Kevin Ott',
+      handicap: 14
+    }, { 
+      id: 23, 
+      name: 'Kirt Dooley',
+      handicap: 12
+    }]
+  },{ 
+    id: 12, 
+    rank: 1,
+    players:[{ 
+      id: 24, 
+      name: 'Cary Ginter',
+      handicap: 8
+    }, { 
+      id: 25, 
+      name: 'Sean Cooper',
+      handicap: 8
+    }]
+  },{ 
+    id: 13, 
+    rank: 1,
+    players:[{ 
+      id: 26, 
+      name: 'Damon Raben',
+      handicap: 8
+    }, { 
+      id: 27, 
+      name: 'Rocky Latronica',
+      handicap: 12
+    }]
+  },{ 
+    id: 14, 
+    rank: 1,
+    players:[{ 
+      id: 28, 
+      name: 'John Oplinger',
+      handicap: 10
+    }, { 
+      id: 29, 
+      name: 'Tom Daley',
+      handicap: 7
+    }]
+  },{ 
+    id: 15, 
+    rank: 1,
+    players:[{ 
+      id: 30, 
+      name: 'Harry McAllister',
+      handicap: 11
+    }, { 
+      id: 31, 
+      name: 'Gerald Ordoyne',
+      handicap: 18
+    }]
+  },{ 
+    id: 16, 
+    rank: 1,
+    players:[{ 
+      id: 32, 
+      name: 'Chris Brown',
+      handicap: 5
+    }, { 
+      id: 33, 
+      name: 'Melissa Ames',
+      handicap: 16
+    }]
+  },{ 
+    id: 17, 
+    rank: 1,
+    players:[{ 
+      id: 34, 
+      name: 'Kerry Johnson',
+      handicap: 5
+    }, { 
+      id: 35, 
+      name: 'Michael Zaguirre',
+      handicap: 5
+    }]
+  },{ 
+    id: 18, 
+    rank: 1,
+    players:[{ 
+      id: 36, 
+      name: 'John Falcone',
+      handicap: 18
+    }, { 
+      id: 37, 
+      name: 'Ovidio Calvet',
+      handicap: 18
+    }]
+  }]);
+}
