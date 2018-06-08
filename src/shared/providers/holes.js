798f4cc1f7c3001f4339efe6ffbf5297217e735d
Ovidio R. Calvet <calveto@gmail.com>
2018-06-07 21:55:03 -0400

    add holes provider

---
+++ src/shared/providers/holes.js
@@ -0,0 +1,93 @@
+export const getHoles = () => {
+  return Promise.resolve([{
+    id: 1,
+    par: 5,
+    handicap: 7,
+    location: 'Front'
+  },{
+    id: 2,
+    par: 4,
+    handicap: 5,
+    location: 'Front'
+  },{
+    id: 3,
+    par: 3,
+    handicap: 17,
+    location: 'Front'
+  },{
+    id: 4,
+    par: 4,
+    handicap: 1,
+    location: 'Front'
+  },{
+    id: 5,
+    par: 4,
+    handicap: 15,
+    location: 'Front'
+  },{
+    id: 6,
+    par: 3,
+    handicap: 11,
+    location: 'Front'
+  },{
+    id: 7,
+    par: 4,
+    handicap: 3,
+    location: 'Front'
+  },{
+    id: 8,
+    par: 4,
+    handicap: 9,
+    location: 'Front'
+  },{
+    id: 9,
+    par: 5,
+    handicap: 13,
+    location: 'Front'
+  },{
+    id: 10,
+    par: 4,
+    handicap: 10,
+    location: 'Back'
+  },{
+    id: 11,
+    par: 4,
+    handicap: 14,
+    location: 'Back'
+  },{
+    id: 12,
+    par: 5,
+    handicap: 2,
+    location: 'Back'
+  },{
+    id: 13,
+    par: 3,
+    handicap: 8,
+    location: 'Back'
+  },{
+    id: 14,
+    par: 4,
+    handicap: 16,
+    location: 'Back'
+  },{
+    id: 15,
+    par: 5,
+    handicap: 12,
+    location: 'Back'
+  },{
+    id: 16,
+    par: 4,
+    handicap: 4,
+    location: 'Back'
+  },{
+    id: 17,
+    par: 3,
+    handicap: 18,
+    location: 'Back'
+  },{
+    id: 18,
+    par: 4,
+    handicap: 16,
+    location: 'Back'
+  }]);
+}
