27402472f2f43205e469d72cc8ed34037e5a39c0
Ovidio R. Calvet <calveto@gmail.com>
2018-06-17 19:55:08 -0400

    organize the hole scoring screen

--- src/ScoreMatchPage/PlayerHolePointsForm/PlayerHolePointsForm.js
+++ src/ScoreMatchPage/PlayerHolePointsForm/PlayerHolePointsForm.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import * as _ from 'lodash';
+import { Typography } from '@material-ui/core';
 
 class PlayerHolePointsForm extends React.Component {
   state = {
@@ -12,31 +12,34 @@ class PlayerHolePointsForm extends React.Component {
   render() {
     const { player, maxPoints } = this.props;
     const menuItems = _
-        .range(1, maxPoints + 1)
-        .map(p => <MenuItem key={p} value={p}>{p}</MenuItem>);
+      .range(1, maxPoints + 1)
+      .map(p => <MenuItem key={p} value={p}>{p}</MenuItem>);
     return (
-      <form
-        // className={classes.root} 
-        autoComplete="off"
-      >
-      <h4 style={{ marginBottom: 0, marginTop: 17 }}>{player.name}</h4>
-        <FormControl
-        // className={classes.formControl}
+      <div style={{ display: 'flex', flexDirection: 'row', marginTop: 15, marginBottom: 15}}>
+        <Typography variant="subheading">{player.name}</Typography>
+        <div style={{ flex: 1 }}></div>
+        <form
+          // className={classes.root} 
+          autoComplete="off"
         >
-          <InputLabel htmlFor="score">Score</InputLabel>
-          <Select
-            value={this.state.score}
-            onChange={(e) => this.handleChange(e)}
-            inputProps={{
-              name: 'score',
-              id: 'score',
-            }}
+          <FormControl
+          // className={classes.formControl}
           >
-            {menuItems}
-            <MenuItem value={0}>Pick Up</MenuItem>
-          </Select>
-        </FormControl>
-      </form>
+            {/* <InputLabel htmlFor="score">Score</InputLabel> */}
+            <Select
+              value={this.state.score}
+              onChange={(e) => this.handleChange(e)}
+              inputProps={{
+                name: 'score',
+                id: 'score',
+              }}
+            >
+              {menuItems}
+              <MenuItem value={0}>Pick Up</MenuItem>
+            </Select>
+          </FormControl>
+        </form>
+      </div>
     )
   }
 }
