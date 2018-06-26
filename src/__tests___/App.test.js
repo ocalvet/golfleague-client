d2632142d51dce445537ab247c566c9936f82ba0
Ovidio R. Calvet <calveto@gmail.com>
2018-06-25 23:18:00 -0400

    Move tests to __tests__ folder

--- src/__tests___/App.test.js
+++ src/__tests___/App.test.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import Enzyme from 'enzyme';
+import App from '../App';
 import Adapter from 'enzyme-adapter-react-16';
 import configureStore from 'redux-mock-store';
 import { createShallow } from '@material-ui/core/test-utils';
 
-Enzyme.configure({ adapter: new Adapter() });
-
-describe('App', () => {
+describe('<App />', () => {
   let shallow;
   const initialState = {output:100}
   const mockStore = configureStore()
@@ -18,11 +15,7 @@ describe('App', () => {
     store = mockStore(initialState)
     container = createShallow(<App store={store} /> )  
   });
-
-  // it('should work', () => {
-  //   const wrapper = shallow(<App />);
-  // });
-
+  
   it('renders without crashing', () => {
     expect(container.length).toEqual(1);
   });
